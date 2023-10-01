1628fd6885dc60d63811b1a3ef86b32ee6a80ca5
yantavares <yantdo1@gmail.com>
2023-10-01 14:24:50 -0300

    v2

--- trab1/trab1.asm
+++ trab1/trab1.asm
@@ -1,10 +1,7 @@
 .data
-blk_in:     .half 0, 1, 2, 3
-blk_out:    .half 0, 0, 0, 0
-keys:       .half 1, 2, 3, 4, 5, 6
-msg_part:   .asciz "Saida["
-equals:     .asciz "] = "
-newline:    .asciz "\n"
+blk_in:  .half 0, 1, 2, 3
+blk_out: .half 0, 0, 0, 0
+keys:    .half 1, 2, 3, 4, 5, 6
 
 .text
 mul:
@@ -29,24 +26,19 @@ mul_add:
     ret
 
 idea_round:
-    # Save base pointers
-    mv t4, a0   # t4 = blk_in_ptr
-    mv t5, a1   # t5 = blk_out_ptr
-    mv t6, a2   # t6 = key_ptr
-
-    # Load words from t4 into t0-t3
-    lh t0, 0(t4)
-    lh t1, 2(t4)
-    lh t2, 4(t4)
-    lh t3, 6(t4)
+    # Load words from blk_in_ptr into t0-t3
+    lh t0, 0(a0)
+    lh t1, 2(a0)
+    lh t2, 4(a0)
+    lh t3, 6(a0)
 
     # Load the keys into s0-s5
-    lh s0, 0(t6)
-    lh s1, 2(t6)
-    lh s2, 4(t6)
-    lh s3, 6(t6)
-    lh s4, 8(t6)
-    lh s5, 10(t6)
+    lh s0, 0(a2)
+    lh s1, 2(a2)
+    lh s2, 4(a2)
+    lh s3, 6(a2)
+    lh s4, 8(a2)
+    lh s5, 10(a2)
 
     # word1 = mul(word1, *key_ptr++);
     mv a0, t0
@@ -54,52 +46,67 @@ idea_round:
     call mul
     mv t0, a0
 
-    # t2 = word1 ^ word3
-    xor t7, t0, t2
+    # word2 = LSW16(word2 + *key_ptr++);
+    add t1, t1, s1
+    andi t1, t1, 0xffff
 
-    # t2 = mul(t2, *key_ptr++);
-    mv a0, t7
-    mv a1, s4
+    # word3 = LSW16(word3 + *key_ptr++);
+    add t2, t2, s2
+    andi t2, t2, 0xffff
+
+    # word4 = mul(word4, *key_ptr++);
+    mv a0, t3
+    mv a1, s3
     call mul
-    mv t7, a0  # result is stored back in t7
+    mv t3, a0
+    
+    # t2 = word1 ^ word3
+    xor t4, t0, t2
+
+    # t2 = mul(t2, *key_ptr++)
+    mv a0, t4
+    lh a1, 12(a2)    # Load next key
+    call mul
+    mv t4, a0       # Store the result in t4
 
     # t1 = LSW16(t2 + (word2 ^ word4))
-    xor t8, t1, t3   # t8 = word2 ^ word4
-    add t8, t7, t8   # t8 = t2 + (word2 ^ word4)
-    andi t8, t8, 0xffff  # t8 now holds LSW16(t2 + (word2 ^ word4))
+    xor t5, t1, t3
+    add t5, t5, t4
+    andi t5, t5, 0xffff
 
     # t1 = mul(t1, *key_ptr++)
-    mv a0, t8
-    mv a1, s5
+    mv a0, t5
+    lh a1, 14(a2)    # Load next key
     call mul
-    mv t8, a0  # result is stored back in t8
+    mv t5, a0       # Store the result in t5
 
     # t2 = LSW16(t1 + t2)
-    add t7, t8, t7
-    andi t7, t7, 0xffff  # t7 now holds LSW16(t1 + t2)
+    add t4, t4, t5
+    andi t4, t4, 0xffff
 
     # word1 ^= t1
-    xor t0, t0, t8
+    xor t0, t0, t5
 
     # word4 ^= t2
-    xor t3, t3, t7
+    xor t3, t3, t4
 
     # t2 ^= word2
-    xor t7, t7, t1
+    xor t4, t4, t1
 
     # word2 = word3 ^ t1
-    xor t1, t2, t8
+    xor t1, t2, t5
 
     # word3 = t2
-    mv t2, t7
+    mv t2, t4
 
-    # Store results to t5
-    sh t0, 0(t5)
-    sh t1, 2(t5)
-    sh t2, 4(t5)
-    sh t3, 6(t5)
+    # Store the results to blk_out_ptr
+    sh t0, 0(a1)
+    sh t1, 2(a1)
+    sh t2, 4(a1)
+    sh t3, 6(a1)
 
     ret
+
 
 
 main:
@@ -108,40 +115,20 @@ main:
     la a1, blk_out
     la a2, keys
 
-    # Call the idea_round function
+    # Call idea_round
     call idea_round
 
     # Setup for printing the results
-    li t0, 0      # Loop index
-    la a1, blk_out  # Reset a1 to the start of blk_out
+    li t0, 0     # loop index
+    la a1, blk_out
 
 print_loop:
     # Check if we've printed all four values
     bge t0, 4, end
 
-    # Print "Saida["
-    la a0, msg_part
-    li a7, 4      # Print string
-    ecall
-
-    # Print index
-    mv a0, t0
-    li a7, 34     # Print integer
-    ecall
-
-    # Print "] = "
-    la a0, equals
-    li a7, 4      # Print string
-    ecall
-
-    # Load and print the value from blk_out
-    lh a0, 0(a1)  # Load halfword to a0
-    li a7, 34     # Print integer
-    ecall
-
-    # Print newline
-    la a0, newline
-    li a7, 4      # Print string
+    # Print blk_out[t0]
+    lh a0, 0(a1)  # load halfword to a0
+    li a7, 34     # print integer
     ecall
 
     # Update index and pointer for the next iteration
@@ -151,5 +138,5 @@ print_loop:
 
 end:
     # Exit the program
-    li a7, 10     # Exit syscall
+    li a7, 10     # exit syscall
     ecall
