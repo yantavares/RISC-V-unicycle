f745b7831639c51b6b323903860375d86710bf16
yantavares <yantdo1@gmail.com>
2023-10-24 15:21:53 -0300

    new fibo

--- fibo.cpp
+++ fibo.cpp
@@ -1,8 +1,12 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <chrono>
 
 using namespace std;
 
-int recursiveFibo(int n)
+// Nth Fibonacci number to be computed
+int N = 40;
+
+long long recursiveFibo(long long n)
 {
     if (n < 1)
     {
@@ -18,12 +22,12 @@ int recursiveFibo(int n)
     }
 }
 
-int dynamicFibo(int n)
+long long dynamicFibo(long long n)
 {
-    int fibo[n + 1];
+    long long fibo[n + 1];
     fibo[0] = 0;
     fibo[1] = 1;
-    for (int i = 2; i <= n; i++)
+    for (long long i = 2; i <= n; i++)
     {
         fibo[i] = fibo[i - 1] + fibo[i - 2];
     }
@@ -32,6 +36,17 @@ int dynamicFibo(int n)
 
 int main(int argc, char const *argv[])
 {
-    cout << "Recursive: " << recursiveFibo(4) << "  Dynamic: " << dynamicFibo(4) << endl;
+    auto start_recursive = chrono::high_resolution_clock::now();
+    cout << "Recursive : " << recursiveFibo(N) << "  ";
+    auto end_recursive = chrono::high_resolution_clock::now();
+    auto duration_recursive = chrono::duration_cast<chrono::milliseconds>(end_recursive - start_recursive);
+    cout << "Time taken by Recursive function: " << duration_recursive.count() << " milliseconds" << endl;
+
+    auto start_dynamic = chrono::high_resolution_clock::now();
+    cout << "Dynamic   : " << dynamicFibo(N) << "  ";
+    auto end_dynamic = chrono::high_resolution_clock::now();
+    auto duration_dynamic = chrono::duration_cast<chrono::milliseconds>(end_dynamic - start_dynamic);
+    cout << "Time taken by Dynamic   function: " << duration_dynamic.count() << " milliseconds" << endl;
+
     return 0;
 }
