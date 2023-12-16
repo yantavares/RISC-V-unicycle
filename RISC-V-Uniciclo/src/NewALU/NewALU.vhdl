08f3ca0fb8b9ec2556a647a910e3cb596d34b10c
yantavares <yantdo1@gmail.com>
2023-12-16 16:32:16 -0300

    Refactor ALU opcode processing logic

--- RISC-V-Uniciclo/src/NewALU/NewALU.vhdl
+++ RISC-V-Uniciclo/src/NewALU/NewALU.vhdl
@@ -19,110 +19,117 @@ BEGIN
 
   PROCESS (opcode, A, B)
   BEGIN
-    CASE opcode IS
+    -- Check if opcode is undefined ('UUUU' or similar)
+    IF opcode = "UUUU" THEN
+        -- Skip processing or assign default values
+        Z <= (others => '0');  -- Default or safe value for Z
+        zero <= '0';  -- Default or safe value for zero
+    ELSE
+        -- Original ALU processing
+        CASE opcode IS
+          WHEN "0000" =>
+            Z <= STD_LOGIC_VECTOR(SIGNED(A) + SIGNED(B));
+            zero <= '0';
 
-      WHEN "0000" =>
-        Z <= STD_LOGIC_VECTOR(SIGNED(A) + SIGNED(B));
-        zero <= '0';
-
-      WHEN "0001" =>
-        Z <= STD_LOGIC_VECTOR(SIGNED(A) - SIGNED(B));
-        zero <= '0';
-
-      WHEN "0010" =>
-        Z <= A AND B;
-        zero <= '0';
-
-      WHEN "0011" =>
-        Z <= A OR B;
-        zero <= '0';
-
-      WHEN "0100" =>
-        Z <= A XOR B;
-        zero <= '0';
-
-      WHEN "0101" =>  -- Shift Left Logical
-        -- Set to zero if shift amount is too large
-        Z <= (others => '0') WHEN TO_INTEGER(SIGNED(B)) >= A'LENGTH ELSE
-             STD_LOGIC_VECTOR(SIGNED(A) SLL TO_INTEGER(SIGNED(B)));
-        zero <= '0';
-      
-      WHEN "0110" =>
-        Z <= STD_LOGIC_VECTOR(SIGNED(A) SRL TO_INTEGER(SIGNED(B)));
-        zero <= '0';
-
-      WHEN "0111" =>
-        Z <= STD_LOGIC_VECTOR(SHIFT_RIGHT(SIGNED(A), TO_INTEGER(SIGNED(B))));
-        zero <= '0';
-
-      WHEN "1000" =>
-        IF SIGNED(A) < SIGNED(B) THEN
-          Z <= x"00000001";
-          zero <= '1';
-        ELSE
-          Z <= x"00000000";
+        WHEN "0001" =>
+          Z <= STD_LOGIC_VECTOR(SIGNED(A) - SIGNED(B));
           zero <= '0';
-        END IF;
 
-      WHEN "1001" =>
-        IF SIGNED(A) < SIGNED(B) THEN
-          Z <= x"00000001";
-          zero <= '1';
-        ELSE
-          Z <= x"00000000";
+        WHEN "0010" =>
+          Z <= A AND B;
           zero <= '0';
-        END IF;
 
-      WHEN "1010" =>
-        IF SIGNED(A) < SIGNED(B) THEN
-          Z <= x"00000000";
+        WHEN "0011" =>
+          Z <= A OR B;
           zero <= '0';
-        ELSE
-          Z <= x"00000001";
-          zero <= '1';
-        END IF;
 
-      WHEN "1011" =>
-        IF SIGNED(A) < SIGNED(B) THEN
-          Z <= x"00000000";
+        WHEN "0100" =>
+          Z <= A XOR B;
           zero <= '0';
-        ELSE
-          Z <= x"00000001";
-          zero <= '1';
-        END IF;
 
-      WHEN "1100" =>
-        IF SIGNED(A) = SIGNED(B) THEN
-          Z <= x"00000001";
-          zero <= '1';
-        ELSE
-          Z <= x"00000000";
+        WHEN "0101" =>  -- Shift Left Logical
+          -- Set to zero if shift amount is too large
+          Z <= (others => '0') WHEN TO_INTEGER(SIGNED(B)) >= A'LENGTH ELSE
+              STD_LOGIC_VECTOR(SIGNED(A) SLL TO_INTEGER(SIGNED(B)));
           zero <= '0';
-        END IF;
-
-      WHEN "1101" =>
-        IF SIGNED(A) = SIGNED(B) THEN
-          Z <= x"00000000";
+        
+        WHEN "0110" =>
+          Z <= STD_LOGIC_VECTOR(SIGNED(A) SRL TO_INTEGER(SIGNED(B)));
           zero <= '0';
-        ELSE
-          Z <= x"00000001";
-          zero <= '1';
-        END IF;
 
-      WHEN "1110" =>
-        Z <= STD_LOGIC_VECTOR(SIGNED(B));
-        zero <= '0';
+        WHEN "0111" =>
+          Z <= STD_LOGIC_VECTOR(SHIFT_RIGHT(SIGNED(A), TO_INTEGER(SIGNED(B))));
+          zero <= '0';
 
-      WHEN "1111" =>
-        Z <= STD_LOGIC_VECTOR(SIGNED(A) + SIGNED(B));
-        zero <= '0';
+        WHEN "1000" =>
+          IF SIGNED(A) < SIGNED(B) THEN
+            Z <= x"00000001";
+            zero <= '1';
+          ELSE
+            Z <= x"00000000";
+            zero <= '0';
+          END IF;
 
-      WHEN OTHERS =>
-        report "Invalid opcode" & to_string(opcode) severity error;
-        Z <= "00000000000000000000000000000000";
-        zero <= '0';
+        WHEN "1001" =>
+          IF SIGNED(A) < SIGNED(B) THEN
+            Z <= x"00000001";
+            zero <= '1';
+          ELSE
+            Z <= x"00000000";
+            zero <= '0';
+          END IF;
 
-    END CASE;
+        WHEN "1010" =>
+          IF SIGNED(A) < SIGNED(B) THEN
+            Z <= x"00000000";
+            zero <= '0';
+          ELSE
+            Z <= x"00000001";
+            zero <= '1';
+          END IF;
+
+        WHEN "1011" =>
+          IF SIGNED(A) < SIGNED(B) THEN
+            Z <= x"00000000";
+            zero <= '0';
+          ELSE
+            Z <= x"00000001";
+            zero <= '1';
+          END IF;
+
+        WHEN "1100" =>
+          IF SIGNED(A) = SIGNED(B) THEN
+            Z <= x"00000001";
+            zero <= '1';
+          ELSE
+            Z <= x"00000000";
+            zero <= '0';
+          END IF;
+
+        WHEN "1101" =>
+          IF SIGNED(A) = SIGNED(B) THEN
+            Z <= x"00000000";
+            zero <= '0';
+          ELSE
+            Z <= x"00000001";
+            zero <= '1';
+          END IF;
+
+        WHEN "1110" =>
+          Z <= STD_LOGIC_VECTOR(SIGNED(B));
+          zero <= '0';
+
+        WHEN "1111" =>
+          Z <= STD_LOGIC_VECTOR(SIGNED(A) + SIGNED(B));
+          zero <= '0';
+
+        WHEN OTHERS =>
+          report "Invalid opcode" & to_string(opcode) severity error;
+          Z <= "00000000000000000000000000000000";
+          zero <= '0';
+
+      END CASE;
+    END IF;
   END PROCESS;
 
 END bdf_type;
